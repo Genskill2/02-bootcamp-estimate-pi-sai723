c17c7ddddac645d2687b5ea5b4183bcedd672b61
sai723 <85287484+sai723@users.noreply.github.com>
2021-06-06 19:29:14 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,28 @@
 import math
 import unittest
 
+def wallis(n):
+    prod = 1
+    for i in range(1,n+1):
+        a = (4*(i**2))/((4*(i**2)) - 1)
+        prod*=a
+    return(2*prod)
+    
+from random import *
+def monte_carlo(n):
+    nod_circle = 0      # number of dots inside cirle
+    nod_square = 0      # number of dots inside square
+    for i in range(n):
+        x = random()
+        y = random()
+        if (x**2 + y**2)<=1:
+            nod_circle+=1
+            nod_square+=1
+        else:
+            nod_square+=1
+    
+    return(4*nod_circle/nod_square)
+
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
